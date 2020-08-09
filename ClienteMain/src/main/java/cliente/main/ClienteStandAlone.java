b45c2b6ab6c43259bc3b28e13ee946a4cae65256
Bernardo Fagalde <bfagalde@bps.gub.uy>
2020-08-08 22:52:41 -0300

    proyectpo nuevo, solo main

---
+++ ClienteMain/src/main/java/cliente/main/ClienteStandAlone.java
@@ -0,0 +1,12 @@
+package cliente.main;
+
+import java.util.Date;
+
+public class ClienteStandAlone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, dice berni: " + new Date());
+	}
+
+}
